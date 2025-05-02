8543fadc07e8c9a6eb4e6377aa6133592d35a79c
MovemDimon <movem9013@gmail.com>
2025-05-02 13:24:12 +0330

    Update main.py

--- main.py
+++ main.py
@@ -80,4 +80,4 @@ async def notify_user(payload: dict):
     return {"status": "user_not_connected"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
